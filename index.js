ced21d0789572d4c79d362e8beeaf1149e0007f3
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-11-29 13:05:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    // code solution
+  
+    const form = document.querySelector('form');
+    //console.log(form);
+    form.addEventListener('submit', function(event){
+    event.preventDefault();
+    let description = document.getElementById("new-task-description").value;
+  
+    // let ul = document.getElementById("tasks");
+    // console.log(ul);
+    // ul.appendChild(description);
+    //let div = document.createElement("div")
+
+    let div = document.getElementById("list");
+    let p = document.createElement("p")
+    div.append(p)
+    p.innerText = description
+    
+    //console.log(div);
+  
+  })
+  
+  });
